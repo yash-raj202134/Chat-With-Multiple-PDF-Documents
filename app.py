303e1aa66090f4b25635e8754a50065abf7f95eb
yash-raj202134 <yashraj3376@gmail.com>
2024-03-22 18:43:58 +0530

    user app

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
 import utils as ut
+import streamlit as st
+
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        ut.user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = ut.get_pdf_text(pdf_docs)
+                text_chunks = ut.get_text_chunks(raw_text)
+                ut.get_vector_store(text_chunks)
+                st.success("Done")
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-
+''' all utility functions here'''
+import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
@@ -63,10 +64,11 @@ def get_conversational_chain():
 
     return chain
 
+
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
@@ -78,3 +80,4 @@ def user_input(user_question):
 
     print(response)
     st.write("Reply: ", response["output_text"])
+
